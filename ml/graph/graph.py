bb9b2d3d188294bcf35af8e4c50a55994c8a4132
sahilbelurkar810 <sahilbelurkar28@gmail.com>
2024-12-20 17:34:00 +0530

    20-12-2024

--- ml/graph/graph.py
+++ ml/graph/graph.py
@@ -1,19 +1,18 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-
-# giwe x y list
-#make x random
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-y = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
-
+y = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 fig,ax = plt.subplots(facecolor='brown')
 # plt.scatter(x, y,color="red")
-plt.plot(x, y, label='data',color="green",marker="*",linestyle=":",linewidth=2)
-plt.xlabel("x")
-plt.ylabel("y")
+plt.plot(x, y, label='Line 1',color="green",marker="2",linestyle=":",linewidth=2,markerfacecolor="red",markeredgecolor="yellow")
+plt.plot(y, x, label='Line 2',color="blue",marker="1",linestyle="--",linewidth=2,markerfacecolor="orange",markeredgecolor="purple")
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
 plt.title("Scatter Plot")
 # plt.figure(figsize=(10, 5))
 plt.grid(True)
 plt.gca().set_facecolor('black')
+plt.legend()
 plt.show()
